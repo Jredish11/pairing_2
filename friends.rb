474e3e2f3ff2bc1732e263c8897dac64d21d066f
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-09-14 16:14:36 -0600

    Add joe

--- friends.rb
+++ friends.rb
@@ -2,9 +2,8 @@ friend = ["Jeff"]
 friend2 = ["Joe"]
 
 if friend == "Jeff"
-  puts "Hi, buddy!"
-elsif friend != "Jeff"
-  puts "Sorry your not Jeff"
-if friend2 == "Joe"
-  puts "Hi Joe!"
+  puts "Hi buddy"
+elsif friend2 != "Jeff"
+  puts "Hi Joe"
 end
+  
